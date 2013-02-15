91f0fc74e1aaca6a8d9745acc3f0d70e3d84b489
Ravish Malik <ray1claw@gmail.com>
2013-02-15 19:04:21 +0530

    'nav-hide on click' done

--- app/scripts/custom.js
+++ app/scripts/custom.js
@@ -1,17 +1,15 @@
-$('.nav a').on('click', function() {
+hide_nav =  function() {
 	if($('.btn-navbar').is(":visible"))
 		$('.btn-navbar').click();
-});
+};
 
 $("html").keydown(function(event) {
-  if(event.keyCode==37){
-  	$(".page-controls .left").click();
-  }
-  if(event.keyCode==39){
-  	$(".page-controls .right").click();
-  }
+	if(event.keyCode==37){
+  		$(".page-controls .left").click();
+	}
+	if(event.keyCode==39){
+  		$(".page-controls .right").click();
+	}
 });
 
-$(document).ready(function($) {
-	$('#about-content').fitVids();
-});
+$('#about-content').fitVids();
