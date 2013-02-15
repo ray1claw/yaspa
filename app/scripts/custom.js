b02717f278661495bb548395e2408a7f8d98063d
Ravish Malik <ray1claw@gmail.com>
2013-02-15 07:49:02 +0530

    navbar close on page change functionality added
    and also wasted a lot of time playing around with coffeescript :(

--- app/scripts/custom.js
+++ app/scripts/custom.js
@@ -1,11 +1,4 @@
-(function() {
-
-  $('.nav a').click(function(e) {
-    e.preventDefault();
-    $(this).tab('show');
-    if ($('.btn-navbar').is(":visible")) {
-      return $('.btn-navbar').click();
-    }
-  });
-
-}).call(this);
+$('.nav a').on('click', function() {
+	if($('.btn-navbar').is(":visible"))
+		$('.btn-navbar').click();
+});
