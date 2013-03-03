714c0d657e4429aa2a57e63657fa29b756b5cf48
fotuzlab <arijitdutta@fotuzlab.in>
2013-03-03 19:31:38 +0530

    replaced files with original angular-express-seed

---
+++ public/js/app.js
@@ -0,0 +1,10 @@
+'use strict';
+
+
+// Declare app level module which depends on filters, and services
+angular.module('myApp', ['myApp.filters', 'myApp.services', 'myApp.directives']).
+  config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
+    $routeProvider.when('/home', {templateUrl: 'partials/home', controller: MyCtrl1});
+    $routeProvider.otherwise({redirectTo: '/home'});
+    $locationProvider.html5Mode(true);
+  }]);
