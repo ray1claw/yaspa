934a0038746f7f27abbeaf1a48efe134c69db5f1
fotuzlab <arijitdutta@fotuzlab.in>
2013-03-05 17:24:58 +0530

    added directive for home route animation

--- public/js/app.js
+++ public/js/app.js
@@ -2,7 +2,7 @@
 
 
 // Declare app level module which depends on filters, and services
-angular.module('myApp', ['myApp.filters', 'myApp.services', 'myApp.directives']).
+var myApp = angular.module('myApp', ['myApp.filters', 'myApp.services', 'myApp.directives']).
   config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
     $routeProvider.when('/home', {templateUrl: 'partials/home', controller: MyCtrl1});
     $routeProvider.otherwise({redirectTo: '/home'});
@@ -12,20 +12,5 @@ angular.module('myApp', ['myApp.filters', 'myApp.services', 'myApp.directives'])
 // load the following through angular after tile load
   $(document).ready(function() {
 
-		// $('li.tile').hide();
-		$('li.tile').css('opacity', '0');
-		$('li.tile').css('margin-left', '-20px');
-    $('ul.tile-group li').each(function(index) {
-
-    	var stallFor = 75 * parseInt(index);
-      
-      // this.css({'opacity':0})   // Set to 0 as soon as possible – may result in flicker, but it's not hidden for users with no JS (Googlebot for instance!)
-      // $(this).delay(stallFor).css('opacity', '1');  // Fade it back in. Swap css for animate in legacy browsers if required.
-      $(this).delay(stallFor).animate({ 'opacity': '+=1', 'margin-left':'+=20px'}, function () {
-            // $(this).animate({ 'opacity': '-=1' });
-    });
-    // return false;
-	});
-
-  $('body').css('width', '5000px');
+		
 });
