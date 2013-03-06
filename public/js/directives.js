96761a47e45e85c8b586c54df78fcb8c7f82011d
fotuzlab <arijitdutta@fotuzlab.in>
2013-03-06 14:00:07 +0530

    removed/added comments

--- public/js/directives.js
+++ public/js/directives.js
@@ -12,22 +12,16 @@ angular.module('myApp.directives', []).
 
 myApp.directive('startAnimation', function(){
   return function(){
+    // Remove link from a.
     $('.tile-group a').removeAttr('href');
-    // $('li.tile').hide();
+    // Add animation.
 		$('li.tile').css('opacity', '0');
 		$('li.tile').css('margin-left', '-20px');
     $('ul.tile-group li').each(function(index) {
-
     	var stallFor = 75 * parseInt(index);
-      
-      // this.css({'opacity':0})   // Set to 0 as soon as possible – may result in flicker, but it's not hidden for users with no JS (Googlebot for instance!)
-      // $(this).delay(stallFor).css('opacity', '1');  // Fade it back in. Swap css for animate in legacy browsers if required.
       $(this).delay(stallFor).animate({ 'opacity': '+=1', 'margin-left':'+=20px'}, function () {
-            // $(this).animate({ 'opacity': '-=1' });
     });
-    // return false;
 	});
-
   $('body').css('width', '5000px');
   }	
 });
